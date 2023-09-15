dafbb20955af2480193ac13d60b38f3f4ae56920
Helen Esterman <helenesterman99@gmail.com>
2023-09-15 12:04:08 -0400

    added some css to RecordArchive to get images same size

--- src/components/recordArchive/RecordArchive.js
+++ src/components/recordArchive/RecordArchive.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getAllEpAlbums } from "../../data/epAlbumData";
 import { getAllLpAlbums } from "../../data/lpAlbumData";
+import "./RecordArchive.css";
 
 export const RecordArchive = () => {
   const [epAlbums, setEpAlbums] = useState([]);
@@ -21,8 +22,28 @@ export const RecordArchive = () => {
         {epAlbums.map((epObj) => {
           return (
             <div className="album" key={epObj.id}>
-              <img src={epObj.imgUrl} alt="album covers" />{" "}
+              <img
+                src={epObj.imgUrl}
+                alt="album covers"
+                width={200}
+                height={200}
+              />
               <div>"{epObj.name}"</div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="album-list">
+        {lpAlbums.map((lpObj) => {
+          return (
+            <div className="album" key={lpObj.id}>
+              <img
+                src={lpObj.imgUrl}
+                alt="album covers"
+                width={200}
+                height={200}
+              />
+              <div>"{lpObj.name}"</div>
             </div>
           );
         })}
